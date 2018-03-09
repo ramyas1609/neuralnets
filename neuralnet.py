faec6926c8380ede3a9e4cfc42f81e72b33151e6
Ramya <ramya@Ramyas-MBP.wv.cc.cmu.edu>
2018-03-09 15:40:59 -0500

    First subission

--- neuralnet.py
+++ neuralnet.py
@@ -1,19 +1,31 @@
 import sys
 import numpy as np
 from math import *
-from decimal import Decimal
-
 
 labels = []
 pixels = []
 
+validation_labels = []
+validation_pixels = []
 
-def read_csv(filename):
+train_entropy = []
+validation_entropy = []
+
+
+def read_train_csv(filename):
     train_csv_file_ = open(filename)
     for line in train_csv_file_.readlines():
         elements = line.rstrip().split(",")
         labels.append(elements[0])
         pixels.append([1] + elements[1:])
+
+
+def read_validation_csv(filename):
+    validation_csv_file_ = open(filename)
+    for line in validation_csv_file_.readlines():
+        elements = line.rstrip().split(",")
+        validation_labels.append(elements[0])
+        validation_pixels.append([1] + elements[1:])
 
 
 def sigmoid_forward(a):
@@ -53,7 +65,7 @@ def crossentropy_forward(a, acap):
 
 
 def crossentropy_backward(a, acap, gb):
-    gacap = np.zeros(shape=i_num_classes)
+    gacap = np.zeros(shape=i_num_classes, dtype=float)
     gacap[a] = -gb / acap[a]
     return gacap
 
@@ -64,9 +76,8 @@ def nnforward(x, y, al, be):
     z1 = np.insert(z, 0, 1.0)
     b = linear_forward(z1, be)
     ycap = softmax_forward(b)
-    #print "ycap", ycap
+    print "ycap", ycap
     j = crossentropy_forward(y, ycap)
-    #print "j", j
     return [a, z1, b, ycap, j]
 
 
@@ -84,50 +95,105 @@ def nnbackward(x, y, be, o):
 
 
 def sgd(num_epoch, hidden_units, init_mode, num_classes):
-    alpha = np.zeros(shape=(hidden_units, 128 + 1), dtype=float)
+    if init_mode == 2:
+        alpha = np.zeros(shape=(hidden_units, 128 + 1), dtype=float)
+        beta = np.zeros(shape=(num_classes, hidden_units + 1), dtype=float)
+    else:
+        alpha = np.random.uniform(-0.1, 0.1, size=(hidden_units, 128 + 1))
+        beta = np.random.uniform(-0.1, 0.1, size=(num_classes, hidden_units + 1))
+
     alpha[:, 0] = 1.0
-    beta = np.zeros(shape=(num_classes, hidden_units + 1), dtype=float)
     beta[:, 0] = 1.0
     for e in xrange(0, num_epoch):
         for i in xrange(0, len(labels)):
-            #print "hi", i, e
             x = np.asarray(pixels[i], dtype=float)
             o = nnforward(x, int(labels[i]), alpha, beta)
             galpha, gbeta = nnbackward(x, int(labels[i]), beta, o)
             alpha = alpha - (i_learning_rate * galpha)
             beta = beta - (i_learning_rate * gbeta)
-            #print i, "galpha", galpha
-            #print i, "gbeta", gbeta
-        calculate_crossentropy(alpha, beta)
+        train_entropy.append(calculate_crossentropy_train(alpha, beta))
+        validation_entropy.append(calculate_crossentropy_validation(alpha, beta))
     return alpha, beta
 
 
-def calculate_crossentropy(al, be):
+def calculate_crossentropy_train(al, be):
     j = 0
     for i in xrange(0, len(labels)):
         x = np.asarray(pixels[i], dtype=float)
         o = nnforward(x, int(labels[i]), al, be)
         j = j + o[4]
     print j/len(labels)
+    return j/len(labels)
 
 
-def fine_diff (x, y, theta):
-    ep = exp(-5)
-    grad = np.zeros(len(theta))
-    for m in xrange(1, len(theta)):
-        d = np.zeros(len(theta))
-        d[m] = 1
-        v = crossentropy_forward()
+def calculate_crossentropy_validation(al, be):
+    j = 0
+    for i in xrange(0, len(validation_labels)):
+        x = np.asarray(validation_pixels[i], dtype=float)
+        o = nnforward(x, int(validation_labels[i]), al, be)
+        j = j + o[4]
+    print j/len(validation_labels)
+    return j/len(validation_labels)
+
+
+def make_prediction_train(al, be):
+    labels_out = open(i_train_labels, "w")
+    num_error = 0
+    for i in xrange(0, len(labels)):
+        x = np.asarray(pixels[i], dtype=float)
+        o = nnforward(x, int(labels[i]), al, be)
+        max_class = np.argmax(o[3])
+        if int(max_class) != int(labels[i]):
+            num_error = num_error + 1
+        labels_out.write(str(int(max_class)))
+        labels_out.write("\n")
+    labels_out.close()
+    return num_error*1.0/len(labels)
+
+
+def make_prediction_validation(al, be):
+    labels_out = open(i_validation_labels, "w")
+    num_error = 0
+    for i in xrange(0, len(validation_labels)):
+        x = np.asarray(validation_pixels[i], dtype=float)
+        o = nnforward(x, int(validation_labels[i]), al, be)
+        max_class = np.argmax(o[3])
+        if int(max_class) != int(validation_labels[i]):
+            num_error = num_error + 1
+        labels_out.write(str(int(max_class)))
+        labels_out.write("\n")
+    labels_out.close()
+    return num_error*1.0/len(validation_labels)
+
+
+def write_metrics_out(filename):
+    metrics_file = open(filename, "w")
+    for i in xrange(0, i_num_epoch):
+        metrics_file.write("epoch=" + str(i + 1) + " crossentropy(train): "+str(train_entropy[i])+"\n")
+        metrics_file.write("epoch=" + str(i + 1) + " crossentropy(validation): " + str(validation_entropy[i]) + "\n")
+
+    metrics_file.write("error(train): "+str(train_error)+"\n")
+    metrics_file.write("error(validation): "+str(validation_error)+"\n")
+    metrics_file.close()
 
 
 i_train_csv = sys.argv[1]
+i_validation_csv = sys.argv[2]
+i_train_labels = sys.argv[3]
+i_validation_labels = sys.argv[4]
+i_metrics = sys.argv[5]
 i_num_epoch = int(sys.argv[6])
 i_hidden_units = int(sys.argv[7])
 i_init_mode = int(sys.argv[8])
 i_learning_rate = float(sys.argv[9])
 i_num_classes = int(10)
 
-read_csv(i_train_csv)
+read_train_csv(i_train_csv)
+read_validation_csv(i_validation_csv)
 
 alpha_1, beta_1 = sgd(i_num_epoch, i_hidden_units, i_init_mode, i_num_classes)
 
+train_error = make_prediction_train(alpha_1, beta_1)
+validation_error = make_prediction_validation(alpha_1, beta_1)
+
+write_metrics_out(i_metrics)
