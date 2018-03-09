97254a511262e8a213049bca828554d3313573a0
Ramya <ramya@Ramyas-MBP.wv.cc.cmu.edu>
2018-03-09 09:37:02 -0500

    Kind of okay

--- neuralnet.py
+++ neuralnet.py
@@ -1,6 +1,8 @@
 import sys
 import numpy as np
 from math import *
+from decimal import Decimal
+
 
 labels = []
 pixels = []
@@ -31,9 +33,9 @@ def softmax_forward(a):
 
 
 def softmax_backward(b, gb):
-    t0 = np.dot(b.transpose(), b)
-    t1 = np.diag(b)
-    return np.dot(gb.transpose(), t1 - t0)
+    t2 = np.diag(b) - np.outer(b, b)
+    t3 = gb.transpose()
+    return np.dot(t3, t2)
 
 
 def linear_forward(a, w):
@@ -41,46 +43,41 @@ def linear_forward(a, w):
 
 
 def linear_backward(a, w, gb):
-    #gw = np.dot(gb[:, None], a[None, :])
-    #ga = np.dot(w.transpose(), gb)
     gw = np.outer(gb, a)
     ga = np.dot(w.transpose(), gb)
     return gw, ga
 
 
 def crossentropy_forward(a, acap):
-    return - (np.log(acap)[a - 1])
+    return -log(acap[a])
 
 
 def crossentropy_backward(a, acap, gb):
-    t0 = gb * 1.0 / acap[a - 1]
     gacap = np.zeros(shape=i_num_classes)
-    gacap[a - 1] = t0
+    gacap[a] = -gb / acap[a]
     return gacap
 
 
 def nnforward(x, y, al, be):
-    print x , al
     a = linear_forward(x, al)
-    print a
     z = sigmoid_forward(a)
     z1 = np.insert(z, 0, 1.0)
     b = linear_forward(z1, be)
     ycap = softmax_forward(b)
+    #print "ycap", ycap
     j = crossentropy_forward(y, ycap)
+    #print "j", j
     return [a, z1, b, ycap, j]
 
 
-def nnbackward(x, y, o):
+def nnbackward(x, y, be, o):
     a = o[0]
     z = o[1]
-    b = o[2]
     ycap = o[3]
-    j = o[4]
     gj = 1.0
     gycap = crossentropy_backward(y, ycap, gj)
     gb = softmax_backward(ycap, gycap)
-    gbe, gz = linear_backward(z, b, gb)
+    gbe, gz = linear_backward(z, be, gb)
     ga = sigmoid_backward(z, gz)
     gal, gx = linear_backward(x, a, ga)
     return gal, gbe
@@ -91,19 +88,36 @@ def sgd(num_epoch, hidden_units, init_mode, num_classes):
     alpha[:, 0] = 1.0
     beta = np.zeros(shape=(num_classes, hidden_units + 1), dtype=float)
     beta[:, 0] = 1.0
-    temp_alpha = np.zeros(shape=(hidden_units, 128 + 1), dtype=float)
-    temp_alpha[:, 0] = 1.0
-    temp_beta = np.zeros(shape=(num_classes, hidden_units + 1), dtype=float)
-    temp_beta[:, 0] = 1.0
     for e in xrange(0, num_epoch):
-        for i in xrange(0, 1):
-            print i
+        for i in xrange(0, len(labels)):
+            #print "hi", i, e
             x = np.asarray(pixels[i], dtype=float)
             o = nnforward(x, int(labels[i]), alpha, beta)
-            galpha, gbeta = nnbackward(x, int(labels[i]), o)
-            alpha = alpha - (0.1 * galpha)
-            beta = beta - (0.1 * gbeta)
+            galpha, gbeta = nnbackward(x, int(labels[i]), beta, o)
+            alpha = alpha - (i_learning_rate * galpha)
+            beta = beta - (i_learning_rate * gbeta)
+            #print i, "galpha", galpha
+            #print i, "gbeta", gbeta
+        calculate_crossentropy(alpha, beta)
     return alpha, beta
+
+
+def calculate_crossentropy(al, be):
+    j = 0
+    for i in xrange(0, len(labels)):
+        x = np.asarray(pixels[i], dtype=float)
+        o = nnforward(x, int(labels[i]), al, be)
+        j = j + o[4]
+    print j/len(labels)
+
+
+def fine_diff (x, y, theta):
+    ep = exp(-5)
+    grad = np.zeros(len(theta))
+    for m in xrange(1, len(theta)):
+        d = np.zeros(len(theta))
+        d[m] = 1
+        v = crossentropy_forward()
 
 
 i_train_csv = sys.argv[1]
@@ -111,10 +125,9 @@ i_num_epoch = int(sys.argv[6])
 i_hidden_units = int(sys.argv[7])
 i_init_mode = int(sys.argv[8])
 i_learning_rate = float(sys.argv[9])
-i_num_classes = int(9)
+i_num_classes = int(10)
 
 read_csv(i_train_csv)
 
 alpha_1, beta_1 = sgd(i_num_epoch, i_hidden_units, i_init_mode, i_num_classes)
 
-print alpha_1,beta_1
